4b6cd469243334789f06dae223a9561875924d04
Jairzijp <jairzijp123@gmail.com>
2023-08-10 14:29:18 +0200

    fetch node by authority

--- src/services/nodes.ts
+++ src/services/nodes.ts
@@ -8,13 +8,24 @@ export class Nodes extends SolanaManager {
 
   /**
    * Function to fetch a node from chain
-   * @param job Publickey address of the node to fetch
+   * @param node Publickey address of the node to fetch
+   * @param authority options, if authority is true it will search node on authority address
    */
-  async get(node: PublicKey | string) : Promise<Node> {
+  async get(node: PublicKey | string, options: { authority: Boolean }) : Promise<Node> {
     if (typeof node === 'string') node = new PublicKey(node);
     await this.loadNosanaNodes();
-    // @ts-ignore:next-line
-    return await this.nodes!.account.nodeAccount.fetch(node);
+
+    if (options && options.authority) {
+      // @ts-ignore:next-line
+      const nodeAccount = await this.nodes!.account.nodeAccount.all([
+        { memcmp: { offset: 8, bytes: node.toString() } },
+      ]);
+      nodeAccount[0].account.address = nodeAccount[0].publicKey.toString();
+      return nodeAccount[0].account;
+    } else {
+      // @ts-ignore:next-line
+      return await this.nodes!.account.nodeAccount.fetch(node);
+    }
   }
 
   /**
