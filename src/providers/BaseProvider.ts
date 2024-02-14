b1c0c96919cb5c4bf3b39fcc9e934fd7df2e1cd4
Laurens Verspeek <l.verspeek@gmail.com>
2024-02-14 17:18:33 +0100

    WIP providers

---
+++ src/providers/BaseProvider.ts
@@ -0,0 +1,35 @@
+
+export type OperationType = "container/run" | "container/create-volume";
+export type JobType = "docker";
+export type Operation = {
+  op: OperationType;
+  id: string;
+  args?: { [key: string]: any };
+}
+export type OperationResult = {
+  id: string;
+  startTime: number;
+  endTime: number;
+  exitCode: number;
+  logs: Array<{
+    type: "stdin" | "stdout" | "stderr";
+    log: string;
+  }>
+  args?: { [key: string]: any };
+}
+
+export type JobDefinition = {
+  version: string;
+  type: JobType;
+  trigger?: string;
+  ops: Array<Operation>;
+}
+
+export type Result = {
+  status: string;
+  ops: Array<OperationResult>;
+}
+
+export abstract class Provider {
+  abstract run(JobDefinition: JobDefinition): Result;
+}
