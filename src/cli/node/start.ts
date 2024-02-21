b4feee2a6b5dfa9e1fb9920f34006b52705eee6c
Laurens Verspeek <l.verspeek@gmail.com>
2024-02-21 15:12:21 +0100

    retrieve job + retrieve job definition + run job + upload results + finish job

--- src/cli/node/start.ts
+++ src/cli/node/start.ts
@@ -1,20 +1,23 @@
 import { Command } from 'commander';
-import { DockerProvider } from '../../providers/DockerProvider';
-import { JobDefinition } from '../../providers/BaseProvider';
-
-const jobDefinition: JobDefinition = {
-  version: "0.1",
-  type: "docker",
-  trigger: "cli",
-  ops: [{
-    type: 'container/run',
-    id: 'run-from-cli',
-    args: {
-      cmds: ["/bin/bash -c 'for i in {1..10}; do echo $i; sleep 1; done'"],
-      image: 'ubuntu',
-    },
-  }]
-};
+import { Client, Job, Market, Run } from '@nosana/sdk';
+import { getSDK } from '../../services/sdk.js';
+import chalk from 'chalk';
+import ora from 'ora';
+import { sleep, clearLine } from '../../generic/utils.js';
+import {
+  getRun,
+  checkQueued,
+  waitForRun,
+  NodeStats,
+  getNodeStats,
+} from '../../services/nodes.js';
+import { NotQueuedError } from '../../generic/errors.js';
+import { DockerProvider } from '../../providers/DockerProvider.js';
+import {
+  BaseProvider,
+  JobDefinition,
+  Result,
+} from '../../providers/BaseProvider.js';
 
 export async function startNode(
   market: string,
@@ -23,18 +26,131 @@ export async function startNode(
   },
   cmd: Command | undefined,
 ) {
-  let provider;
+  /*************
+   * Bootstrap *
+   *************/
+  const nosana: Client = getSDK();
+  const node = nosana.solana.provider!.wallet.publicKey.toString();
+
+  let provider: BaseProvider;
   switch (options.provider) {
-    case "docker":
-      provider = new DockerProvider(options.host, options.port);
-      break;
+    case 'docker':
     default:
       provider = new DockerProvider(options.host, options.port);
+      break;
   }
 
-  if (await provider.healthy()) {
-    await provider.run(jobDefinition);
-  } else {
-    console.log('abort');
+  /****************
+   * Health Check *
+   ****************/
+  const stats: NodeStats = await getNodeStats(node);
+  if (stats.sol < 0.001) throw new Error('not enough SOL');
+  if (!(await provider.healthy())) throw new Error('Provider not healthy');
+  // TODO Check stake account
+  //      If no stake account: create empty stake account
+  // const stake = await nosana.stake.create(
+  //   nodeKey,
+  //   0,
+  //   14,
+  // );
+
+  // TODO Check NFT that is needed for market
+
+  /****************
+   *   Job Loop   *
+   ****************/
+  let spinner = ora(chalk.cyan('Checking existing runs')).start();
+
+  // Check if we already have a run account
+  let run: Run | void = await getRun(node);
+
+  if (!run) {
+    spinner.text = chalk.cyan('Checking queued status');
+    let selectedMarket: Market | void = await checkQueued(node);
+
+    if (!selectedMarket || selectedMarket.address.toString() === market) {
+      if (selectedMarket) {
+        // TODO: We are in the wrong market, leave queue
+        throw new Error('Queued in wrong market, please leave market first');
+      }
+      spinner.text = chalk.cyan('Joining market ');
+      const tx = await nosana.jobs.work(market);
+      console.log(chalk.greenBright(`Joined market tx ${tx}`));
+    }
+    if (selectedMarket) {
+      // Currently queued in a market, wait for run
+      spinner.color = 'yellow';
+      const queuedMarketText = (market: Market, node: string) => {
+        return (
+          chalk.bgYellow.bold(' QUEUED ') +
+          ` at position ${
+            market.queue.findIndex((e: any) => e.toString() === node) + 1
+          }/${market.queue.length} in market ${chalk.cyan.bold(market.address)}`
+        );
+      };
+      spinner.text = queuedMarketText(selectedMarket, node);
+      try {
+        // will only return on a new run account
+        run = await waitForRun(
+          node,
+          selectedMarket.address,
+          // This callback gets called every minute with the updated market
+          (market: Market) => {
+            selectedMarket = market;
+            spinner.text = queuedMarketText(selectedMarket, node);
+          },
+        );
+      } catch (e) {
+        if (e instanceof NotQueuedError) {
+          spinner.warn('Node left market queue..');
+          for (let timer = 10; timer > 0; timer--) {
+            spinner.start(chalk.cyan(`Checking again in ${timer}`));
+            await sleep(1);
+          }
+          spinner.stop();
+          clearLine();
+        } else {
+          throw e;
+        }
+      }
+    }
   }
+  if (run) {
+    if (spinner) spinner.stop();
+    const jobAddress = run.account.job.toString();
+    console.log(chalk.green('Claimed job ') + chalk.green.bold(jobAddress));
+    const job: Job = await nosana.jobs.get(jobAddress);
+    if (job.market.toString() !== market) {
+      // TODO: stop job
+    } else {
+      spinner = ora(chalk.cyan('Retrieving job definition')).start();
+      // TODO: check job expired
+      const jobDefinition: JobDefinition = await nosana.ipfs.retrieve(
+        job.ipfsJob,
+      );
+      spinner.text = chalk.cyan('Checking provider health');
+      if (!(await provider.healthy())) {
+        throw new Error('provider not healthy');
+        // TODO: wait for provider to get healthy
+      }
+      spinner.text = chalk.cyan('Running job');
+      const result: Result = await provider.run(jobDefinition);
+      spinner.text = chalk.cyan('Uploading results to IPFS');
+      const ipfsResult = await nosana.ipfs.pin(result);
+      const bytesArray = nosana.ipfs.IpfsHashToByteArray(ipfsResult);
+      spinner.text = chalk.cyan('Finishing job');
+      const tx = await nosana.jobs.submitResult(
+        bytesArray,
+        run.publicKey,
+        job.market.toString(),
+      );
+      spinner.succeed(chalk.green('Job finished ') + chalk.green.bold(tx));
+      for (let timer = 30; timer > 0; timer--) {
+        spinner.start(chalk.cyan(`Starting again in ${timer}`));
+        await sleep(1);
+      }
+      spinner.stop();
+    }
+  }
+  return startNode(node, options, cmd);
 }
