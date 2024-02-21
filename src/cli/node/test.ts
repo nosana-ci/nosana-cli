1e013b8ef4976890e26e8f28d302cf241ef983ca
Laurens Verspeek <l.verspeek@gmail.com>
2024-02-21 15:12:32 +0100

    old start moved to test

---
+++ src/cli/node/test.ts
@@ -0,0 +1,42 @@
+import { Command } from 'commander';
+import { DockerProvider } from '../../providers/DockerProvider';
+import { JobDefinition } from '../../providers/BaseProvider';
+
+const jobDefinition: JobDefinition = {
+  version: '0.1',
+  type: 'docker',
+  trigger: 'cli',
+  ops: [
+    {
+      type: 'container/run',
+      id: 'run-from-cli',
+      args: {
+        cmds: ["/bin/bash -c 'for i in {1..10}; do echo $i; sleep 1; done'"],
+        image: 'ubuntu',
+      },
+    },
+  ],
+};
+
+export async function startNode(
+  market: string,
+  options: {
+    [key: string]: any;
+  },
+  cmd: Command | undefined,
+) {
+  let provider;
+  switch (options.provider) {
+    case 'docker':
+      provider = new DockerProvider(options.host, options.port);
+      break;
+    default:
+      provider = new DockerProvider(options.host, options.port);
+  }
+
+  if (await provider.healthy()) {
+    await provider.run(jobDefinition);
+  } else {
+    console.log('abort');
+  }
+}
