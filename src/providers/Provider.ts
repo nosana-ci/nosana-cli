6ada216e852412509686f1686f1b878a77b62817
Matthew Hammond <ultramatt96@hotmail.co.uk>
2024-03-30 19:12:56 +0000

    chore: revert entrypoint to optional string array

--- src/providers/Provider.ts
+++ src/providers/Provider.ts
@@ -39,7 +39,7 @@ export interface OperationArgsMap {
     ];
     gpu?: boolean;
     work_dir?: string;
-    entrypoint?: string[] | string;
+    entrypoint?: string[];
     env?: {
       [key: string]: string;
     };
