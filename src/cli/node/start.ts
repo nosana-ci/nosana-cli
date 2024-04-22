9caf5adcd829803219a5a18377a172d8ed52b8cb
Jairzijp <jairzijp123@gmail.com>
2024-04-22 18:07:12 +0200

    add node onboard check, and if no access key, call onboard endpoint

--- src/cli/node/start.ts
+++ src/cli/node/start.ts
@@ -122,6 +122,55 @@ export async function startNode(
       throw e;
     }
   }
+
+  // Check if node is onboarded and has received access key
+  // if not call onboard endpoint to create access key tx
+  // TODO: also do the market check here? Or add this to the market check
+  try {
+    const response = await fetch(
+      `${envConfig.get('BACKEND_URL')}/nodes/${node}`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      },
+    );
+    const nodeResponse = await response.json();
+    if (!nodeResponse) throw new Error('Failed to fetch node info');
+    if (nodeResponse.status === 'onboarded' && !nodeResponse.accessKeyMint) {
+      const response = await fetch(
+        `${envConfig.get('BACKEND_URL')}/nodes/onboard`,
+        {
+          method: 'POST',
+          headers: {
+            Authorization: envConfig.get('ADMIN_KEY'),
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            address: node,
+          }),
+        },
+      );
+      const onboardResponse = await response.json();
+      if (!onboardResponse) throw new Error('Something went wrong onboarding');
+      if (onboardResponse.market) {
+        console.log(
+          chalk.cyan(`Node onboarded in market ${onboardResponse.market}`),
+        );
+      }
+    } else if (nodeResponse.status !== 'onboarded')
+      throw new Error('Node not onboarded yet');
+  } catch (e: unknown) {
+    if (e instanceof Error && e.message.includes('Node not onboarded yet')) {
+      throw new Error(
+        "Node is onboarded yet. You'll receive an email once you are onboarded.",
+      );
+    } else {
+      console.log(e);
+    }
+  }
+
   /****************
    * Health Check *
    ****************/
