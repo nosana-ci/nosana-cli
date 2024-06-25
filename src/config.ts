d5c9d995d0b7cc0c072734faabd3c495bdd4df2b
Matthammond96 <ultramatt96@hotmail.co.uk>
2024-06-25 21:11:44 +0100

    convert config to environment variables

--- src/config.ts
+++ src/config.ts
@@ -8,16 +8,16 @@ export type configType = {
   };
   minDiskSpace: number;
 };
-export const getConfig = async (): Promise<configType> => {
-  try {
-    return (
-      (await import(
-        `./config/${
-          process.env.APP_ENV || process.env.NODE_ENV || 'production'
-        }.ts`
-      )) as { config: configType }
-    ).config;
-  } catch (err) {
-    throw err;
-  }
+
+// TODO: Correct process.env types
+
+export const config: configType = {
+  backendUrl: process.env.BACKEND_URL!,
+  backendSolanaAddress: process.env.BACKEND_SOLANA_ADDRESS!,
+  signMessage: process.env.SIGN_MESSAGE!,
+  frp: {
+    serverAddr: process.env.FRP_SERVER_ADDRESS!,
+    serverPort: parseInt(process.env.FRP_SERVER_PORT!),
+  },
+  minDiskSpace: parseInt(process.env.MIN_DISK_SPACE!),
 };
