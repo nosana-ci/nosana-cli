f91ec4a778f2b6aa4f02c2aaa495d250a86dd429
Matthammond96 <ultramatt96@hotmail.co.uk>
2024-06-25 21:13:20 +0100

    configure ts-jest

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,19 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
-  preset: 'ts-jest/presets/default-esm',
+  preset: 'ts-jest/presets/js-with-ts-esm',
+  testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
   transform: {
-    '\\.[jt]s?$': 'babel-jest',
+    '^.+\\.[tj]sx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+        supportsDynamicImport: true,
+        tsconfig: 'tsconfig.json',
+      },
+    ],
   },
-  transformIgnorePatterns: ['/node_modules/(?!@nosana|chalk)'],
+  transformIgnorePatterns: [
+    'node_modules/(?!@nosana|ora|cli-cursor|restore-cursor|log-symbols|is-unicode-supported|strip-ansi|ansi-regex|string-width|get-east-asian-width|is-interactive|stdin-discarder|lowdb|steno|chalk/.*)',
+  ],
 };
