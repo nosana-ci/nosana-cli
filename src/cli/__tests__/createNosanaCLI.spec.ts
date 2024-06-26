77a8f0cfbe96553bd0aa84684036c011ddbadb61
Matthammond96 <ultramatt96@hotmail.co.uk>
2024-06-26 14:34:41 +0200

    add tests for createNosanaCli commands

--- src/cli/__tests__/createNosanaCLI.spec.ts
+++ src/cli/__tests__/createNosanaCLI.spec.ts
@@ -11,4 +11,21 @@ describe('createNosanaCLI', () => {
     expect(_description).toBe('Nosana CLI');
     expect(_version).toBe('0.0.1');
   });
+
+  it.each([['job', 'node']])('should have %s command', (command) => {
+    // @ts-ignore
+    expect(CLI.commands.map((command) => command._name)).toContain(command);
+  });
+
+  it.each([
+    ['--version', '-V', undefined, true],
+    ['--log', undefined, 'debug', true],
+  ])('should have %s option', (long, short, defaultValue, optional) => {
+    const option = CLI.options.find((i) => i.long === long);
+
+    expect(optional).toEqual(optional);
+    expect(option?.long).toBe(long);
+    expect(option?.short).toBe(short);
+    expect(defaultValue).toBe(defaultValue);
+  });
 });
