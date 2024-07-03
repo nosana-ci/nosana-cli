878f11c8765742ee35a4bd7d3b3a77abe41abcc0
Matthammond96 <ultramatt96@hotmail.co.uk>
2024-07-03 14:29:10 +0100

    bump ci node to v20.11.1 and add test coverage to ci

--- jest.config.js
+++ jest.config.js
@@ -21,4 +21,15 @@ export default {
   transformIgnorePatterns: [
     'node_modules/(?!@nosana|ora|cli-cursor|restore-cursor|log-symbols|is-unicode-supported|strip-ansi|ansi-regex|string-width|get-east-asian-width|is-interactive|stdin-discarder|lowdb|steno|chalk/.*)',
   ],
+  collectCoverageFrom: ['src/**/*.ts'],
+  coverageReporters: ['text', 'text-summary'],
+  coveragePathIgnorePatterns: ['src/types/*'],
+  coverageThreshold: {
+    global: {
+      statements: 0,
+      branches: 0,
+      lines: 0,
+      functions: 0,
+    },
+  },
 };
