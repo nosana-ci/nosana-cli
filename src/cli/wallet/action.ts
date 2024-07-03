183fbc0c536b38e62b7456c1073702fc24a4e660
Matthammond96 <ultramatt96@hotmail.co.uk>
2024-07-03 15:59:00 +0100

    clean up wallet getAddress action

--- src/cli/wallet/action.ts
+++ src/cli/wallet/action.ts
@@ -1,13 +1,5 @@
-import { Command } from 'commander';
-import { type Client } from '@nosana/sdk';
-
 import { getSDK } from '../../services/sdk.js';
 
-export async function getAddress(
-  options: {
-    [key: string]: any;
-  },
-  cmd: Command,
-) {
-  const nosana: Client = getSDK();
+export async function getAddress() {
+  getSDK();
 }
