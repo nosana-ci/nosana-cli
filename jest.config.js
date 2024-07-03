516b3cbfb3651181753d413c56fcf278c0fc3977
Matthammond96 <ultramatt96@hotmail.co.uk>
2024-07-03 14:59:13 +0100

    reset unit test coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -26,7 +26,7 @@ export default {
   coveragePathIgnorePatterns: ['src/types/*'],
   coverageThreshold: {
     global: {
-      statements: 100,
+      statements: 0,
       branches: 0,
       lines: 0,
       functions: 0,
