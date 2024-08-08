d3815cab91e8331cd50cb90084b97bd3b25a4db5
Daniel Fayemi <danieltosinfayemi@gmail.com>
2024-08-08 08:47:25 +0100

    feat: add verbose text output format to output factory

--- src/providers/utils/ouput-formatter/OutputFormatterFactory.ts
+++ src/providers/utils/ouput-formatter/OutputFormatterFactory.ts
@@ -1,6 +1,7 @@
 import { JsonOutputFormatter } from "./json/JsonOutputFormatter.js";
 import { OutputFormatter } from "./OutputFormatter.js";
 import { TextOutputFormatter } from "./text/TextOutputFormatter.js";
+import { VerboseTextOutputFormatter } from "./text/VerboseTextOutputFormatter.js";
 
 export class OutputFormatterFactory {
     static createFormatter(format: string): OutputFormatter {
@@ -8,6 +9,8 @@ export class OutputFormatterFactory {
         // add more formats here
         case 'json':
           return new OutputFormatter(new JsonOutputFormatter());
+        case 'verbose:text':
+          return new OutputFormatter(new VerboseTextOutputFormatter());
         case 'text':
         default:
           return new OutputFormatter(new TextOutputFormatter());
