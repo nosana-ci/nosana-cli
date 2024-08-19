203e46e16d84a76a7e97e49ee9f8fc7b73443f25
Daniel Fayemi <danieltosinfayemi@gmail.com>
2024-08-19 13:01:02 +0100

    feat: change flow to use filesystem to import json

--- src/assertImports.ts
+++ src/assertImports.ts
@@ -1,5 +1,17 @@
-import pkg from '../package.json' with { type: 'json' };
-import benchmarkGPU from './static/benchmark-gpu.json' with { type: 'json' };
-import jobDefinition from './static/benchmark.json' with { type: 'json' };
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const customDirname = path.dirname(fileURLToPath(import.meta.url));
+
+function readJsonFileSync(filePath: string) {
+  const absolutePath = path.resolve(customDirname, filePath);
+  const fileContent = fs.readFileSync(absolutePath, 'utf-8');
+  return JSON.parse(fileContent);
+}
+
+const pkg = readJsonFileSync('../package.json');
+const benchmarkGPU = readJsonFileSync('./static/benchmark-gpu.json');
+const jobDefinition = readJsonFileSync('./static/benchmark.json');
 
 export { pkg, benchmarkGPU, jobDefinition };
