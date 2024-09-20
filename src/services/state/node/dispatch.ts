5794dac636f71adcfa53f086668b7ef7c5db5706
Daniel Fayemi <danieltosinfayemi@gmail.com>
2024-09-20 10:03:57 +0100

    feat: add tracked words

--- src/services/state/node/dispatch.ts
+++ src/services/state/node/dispatch.ts
@@ -1,4 +1,5 @@
 import { getNodeStateManager } from './instance.js';
+import { sharedObj } from './shared.js';
 import { NodeState, NodeStateData } from './types.js';
 
 export const dispatch = (
@@ -6,5 +7,11 @@ export const dispatch = (
   data: NodeStateData[NodeState],
 ) => {
   const stateManager = getNodeStateManager();
-  stateManager.updateState(newState, data);
+
+  const sharedData = {
+    ...sharedObj('node', 'market'),
+    ...data, // Allow additional data to override sharedObj if needed
+  };
+
+  stateManager.updateState(newState, sharedData);
 };
