7504eb126ddc7377d7f15fcfe389870791cbf94f
Matthew Hammond <ultramatt96@hotmail.co.uk>
2024-10-30 12:03:36 +0000

    chore: only show gpu ctk warn if valid option

--- src/cli/sharedOptions/--gpu.ts
+++ src/cli/sharedOptions/--gpu.ts
@@ -2,16 +2,17 @@ import chalk from 'chalk';
 import { InvalidOptionArgumentError, Option } from 'commander';
 
 function validateGPUOption(value: string) {
-  console.warn(
-    chalk.yellow(
-      'Setting GPU device, please ensure your nvidia-ctk is correctly configured to generate using device index naming strategy to avoid errors.',
-    ),
-  );
   if (!value.match(new RegExp(/^\d+(,\d+)*$/)) && value !== 'all') {
     throw new InvalidOptionArgumentError(
       'Expected a comma seperated number list or "all"',
     );
   }
+
+  console.warn(
+    chalk.yellow(
+      `Setting GPU device ${value}, please ensure your nvidia-ctk is correctly configured to generate using device index naming strategy to avoid errors.`,
+    ),
+  );
 
   return value;
 }
