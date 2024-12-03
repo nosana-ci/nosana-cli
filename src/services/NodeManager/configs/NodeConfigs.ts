384304f747ea3d48cbafafe99a92522ece68aa93
daniel <danieltosinfayemi@gmail.com>
2024-12-03 04:35:53 -0500

    feat: made config singelton

--- src/services/NodeManager/configs/NodeConfigs.ts
+++ src/services/NodeManager/configs/NodeConfigs.ts
@@ -3,11 +3,16 @@ import { dirname, resolve } from 'path';
 import { fileURLToPath } from 'url';
 
 export class NodeConfigs {
-    constructor() {}
-  
-    static loadVariablesToEnv(options: { [key: string]: any }) {
-      const env = options.network === 'mainnet' ? 'production' : 'dev';
-  
+    private options: { [key: string]: any };
+
+    constructor(options: { [key: string]: any }) {
+      this.options = options
+    }
+
+    loadVariablesToEnv() {
+      const network = this.options.network ? this.options.network : 'mainnet'
+      const env = network === 'mainnet' ? 'production' : 'dev';
+
       const modulePath = dirname(fileURLToPath(import.meta.url));
   
       dotenv.config({
@@ -17,3 +22,16 @@ export class NodeConfigs {
     }
   }
   
+  export class NodeConfigsSingleton {
+    private static instance: NodeConfigs | null = null;
+  
+    private constructor() {}
+  
+    static getInstance(options?: { [key: string]: any }): NodeConfigs {
+      if (!NodeConfigsSingleton.instance) {
+        NodeConfigsSingleton.instance = new NodeConfigs(options || {});
+        NodeConfigsSingleton.instance.loadVariablesToEnv()
+      }
+      return NodeConfigsSingleton.instance;
+    }
+  }
