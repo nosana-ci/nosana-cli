50b4a44ee29e0ea3618a67b3dda1309edbdf8b5d
Matthew Hammond <ultramatt96@hotmail.co.uk>
2024-12-18 12:15:42 +0000

    chore: return result to admin api

--- src/services/NodeManager/node/api/routes/post/node-validation.ts
+++ src/services/NodeManager/node/api/routes/post/node-validation.ts
@@ -30,19 +30,30 @@ export async function postNodeValidation(
     );
   }
 
-  res.status(200).send();
+  const sessionId = res.locals['session_id']!;
+
+  if (sessionId !== 'ADMIN') {
+    res.status(200).send();
+  }
 
   const id = flowHandler.generateRandomId(32);
   flowHandler.start(id, req.body);
 
   try {
     let result = await flowHandler.run(id);
+
+    if (sessionId === 'ADMIN') {
+      res
+        .status(200)
+        .send(parseFloat(result.state.opStates[0].results!['prediction'][0]));
+      return;
+    }
     // @ts-ignore WAITING ON ENDPOINT CREATION + DEFINING THE RESPONSE OBJECT
     await client.POST('/api/benchmarks/submit', {
       body: result,
       params: {
         header: {
-          authorization: sdk.authorization.generate(res.locals['session_id']!, {
+          authorization: sdk.authorization.generate(sessionId, {
             includeTime: true,
           }),
         },
