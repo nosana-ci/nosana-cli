17b0e10a7d958ae885d6fa233e991dd8472f74ce
Matthew Hammond <ultramatt96@hotmail.co.uk>
2025-06-05 15:38:22 +0000

    feat: job validate command

--- src/cli/job/__tests__/index.spec.ts
+++ src/cli/job/__tests__/index.spec.ts
@@ -2,7 +2,7 @@ import { jobCommand } from '../';
 
 describe('jobCommand', () => {
   it('should only have two commands', () => {
-    expect(jobCommand.commands.length).toBe(7);
+    expect(jobCommand.commands.length).toBe(8);
   });
 
   it.each([
@@ -13,6 +13,7 @@ describe('jobCommand', () => {
     ['upload'],
     ['stop'],
     ['extend'],
+    ['validate'],
   ])('should contain %s command', (command) => {
     // @ts-ignore
     expect(jobCommand.commands.map((command) => command._name)).toContain(
