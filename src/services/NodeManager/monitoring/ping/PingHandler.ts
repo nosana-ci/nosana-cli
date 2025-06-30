d367c18cf6488e719778a8bf48deb6b6544ce0fd
daniel <danieltosinfayemi@gmail.com>
2025-06-30 14:32:38 +0200

    feat: update heartbeat feature

--- src/services/NodeManager/monitoring/ping/PingHandler.ts
+++ src/services/NodeManager/monitoring/ping/PingHandler.ts
@@ -16,32 +16,57 @@ export const ping = (() => {
 
 export class PingHandler {
   private intervalId: NodeJS.Timeout | null = null;
+  private intervalSeconds = 30;
 
   constructor() {}
 
-  start() {
+  async start() {
     if (this.intervalId) return;
-    const time = 30; // seconds
-    this.intervalId = setInterval(async () => {
-      try {
-        const sdk = getSDK();
-        const headers = sdk.authorization.generateHeader(
-          configs().signMessage,
-          {
-            includeTime: true,
-          },
-        );
-        headers.append('Content-Type', 'application/json');
 
-        const response = await fetch(`${configs().backendUrl}/ping`, {
-          method: 'POST',
-          headers,
-          body: JSON.stringify({ ping: 'pong' }),
-        });
-      } catch (err) {
-        console.error('Ping failed:', err);
+    await this.ping(); // start immediately to get the interval
+
+    this.intervalId = setInterval(() => {
+      this.ping();
+    }, this.intervalSeconds * 1000);
+  }
+
+  private async ping() {
+    try {
+      const sdk = getSDK();
+      const headers = sdk.authorization.generateHeader(configs().signMessage, {
+        includeTime: true,
+      });
+      headers.append('Content-Type', 'application/json');
+
+      const response = await fetch(`${configs().backendUrl}/api/nodes/heartbeat`, {
+        method: 'POST',
+        headers,
+        body: JSON.stringify({ ping: 'pong' }),
+      });
+
+      if (response.ok) {
+        const json = await response.json();
+
+        if (json.maxHeartbeatsPerDay) {
+          const newInterval = Math.floor((24 * 60 * 60) / json.maxHeartbeatsPerDay);
+          if (newInterval !== this.intervalSeconds) {
+            this.updateInterval(newInterval);
+          }
+        }
       }
-    }, time * 1000);
+    } catch (err) {
+      console.error('Ping failed:', err);
+    }
+  }
+
+  private updateInterval(newSeconds: number) {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+    this.intervalSeconds = newSeconds;
+    this.intervalId = setInterval(() => {
+      this.ping();
+    }, this.intervalSeconds * 1000);
   }
 
   stop() {
