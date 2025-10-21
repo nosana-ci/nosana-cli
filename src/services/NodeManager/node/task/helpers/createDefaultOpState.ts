bdd0201fe641253909715f5d87985705ea5c5a1e
Ben Snaas <ben@nosana.io>
2025-10-21 11:08:47 +0200

    Added group to opstate

--- src/services/NodeManager/node/task/helpers/createDefaultOpState.ts
+++ src/services/NodeManager/node/task/helpers/createDefaultOpState.ts
@@ -3,6 +3,7 @@ import { OperationType, Operation } from '@nosana/sdk';
 export function createDefaultOpState(op: Operation<OperationType>) {
   return {
     operationId: op.id,
+    group: op.execution?.group ?? op.id,
     providerId: null,
     status: 'init',
     startTime: null,
