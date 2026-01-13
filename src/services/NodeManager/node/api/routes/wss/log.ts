c1993e7c9fa8a4d9a384e58624933a9fdbaef8a8
KeesGeerligs <166137421+KeesGeerligs@users.noreply.github.com>
2026-01-13 13:24:15 +0100

    fix: return old logs

--- src/services/NodeManager/node/api/routes/wss/log.ts
+++ src/services/NodeManager/node/api/routes/wss/log.ts
@@ -45,16 +45,16 @@ export function wssTaskManagerLogRoute(
     );
   });
 
-  // // Send historical logs first (optional)
-  // const logs = opId
-  //   ? task.getLogsByOp(opId)
-  //   : group
-  //   ? task.getLogsByGroup(group)
-  //   : task.getAllLogs();
+  // Send historical logs first (optional)
+  const logs = opId
+    ? task.getLogsByOp(opId)
+    : group
+    ? task.getLogsByGroup(group)
+    : task.getAllLogs();
 
-  // for (const log of logs) {
-  //   try {
-  //     ws.send(JSON.stringify({ path: 'flog', data: JSON.stringify(log) }));
-  //   } catch {}
-  // }
+  for (const log of logs) {
+    try {
+      ws.send(JSON.stringify({ path: 'flog', data: JSON.stringify(log) }));
+    } catch {}
+  }
 }
